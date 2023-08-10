cf78bc43b39fffae45481f75c9570c6bc9095b93
Krakou <krakoukasse13@gmail.com>
2023-08-10 22:07:06 +0200

    step 4 & 5 finish

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
 /* Variable*/
 var left = document.getElementById("left");
 var right = document.getElementById("right");
+var img = document.getElementById("img-banner")
+var p = document.getElementById("p-banner")
 const slides = [
 	{
 		"image":"slide1.jpg",
@@ -27,13 +29,17 @@ console.log(right);
 console.log("il y'a dans le slider " + slides.length);
 
 
+
 //On ajoute des evénement au clic
 left.addEventListener("click", () => {alert('Bouton gauche')} );
 right.addEventListener("click", () => {alert('Bouton droite')} );
 
+//
+
 
 // Fonction
-
+left.addEventListener("click", () => dotChange("left"));
+right.addEventListener("click", () => dotChange("right"));
 
 
 function dot_generation(){
@@ -54,3 +60,40 @@ function dot_generation(){
 	}
 
 dot_generation();
+
+function dotChange(direction){
+	let dot = document.getElementsByClassName('dot')
+	console.log(dot)
+	for (var i = 0; i < dot.length; i++) {
+		if (dot[i].classList.contains("dot_selected")) {
+			selectedIndex = i;
+			dot[i].classList.remove("dot_selected")
+			console.log("Avant le +" + selectedIndex)
+			if(direction == "left"){
+				if(selectedIndex == 0){
+					
+					selectedIndex = dot.length -1
+				}
+				else{
+					selectedIndex--
+				}
+			}
+			else{
+				if(selectedIndex == dot.length -1){
+					console.log("Dans la boucle")
+					selectedIndex = 0
+				}
+				else{
+					selectedIndex++
+				}
+				
+			}
+			dot[selectedIndex].classList.add('dot_selected')
+			img.src = "./assets/images/slideshow/" + slides[selectedIndex].image
+			p.innerHTML = slides[selectedIndex].tagLine
+			console.log("Apres " +selectedIndex)
+			break; // Sortir de la boucle une fois que l'élément a été trouvé
+		}
+	}
+}
+	
