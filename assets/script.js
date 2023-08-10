417d5a6bc6470857ef65dd76ebdc1ff72c92733b
Krakou <krakoukasse13@gmail.com>
2023-08-10 17:44:28 +0200

    step 3 dots

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,6 @@
 /* Variable*/
 var left = document.getElementById("left");
 var right = document.getElementById("right");
-
-//On teste si les élements sont detecté
-console.log(left);
-console.log(right);
-
-//On ajoute des evénement au clic
-left.addEventListener("click", () => {alert('Bouton gauche')} );
-right.addEventListener("click", () => {alert('Bouton droite')} );
-
-
-// Fonction
-
-
 const slides = [
 	{
 		"image":"slide1.jpg",
@@ -31,4 +18,39 @@ const slides = [
 		"image":"slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
-]
+];
+
+
+//On teste si les élements sont detecté
+console.log(left);
+console.log(right);
+console.log("il y'a dans le slider " + slides.length);
+
+
+//On ajoute des evénement au clic
+left.addEventListener("click", () => {alert('Bouton gauche')} );
+right.addEventListener("click", () => {alert('Bouton droite')} );
+
+
+// Fonction
+
+
+
+function dot_generation(){
+	for(let i = 0; i < slides.length; i++){
+		// On crée notre balise dot
+		let dot = document.createElement('span');
+		//On ajoute a chacune la class dot
+		dot.classList.add('dot');
+		//On les ajotue dans notre page HTML
+		document.getElementById('div_dots').appendChild(dot);
+		console.log(i);
+		//Condition
+		if(i == 0){
+			dot.classList.add('dot_selected')
+		}
+	}
+	
+	}
+
+dot_generation();
